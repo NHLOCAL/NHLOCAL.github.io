dd9366ef8883322d990f893dab5f3c5f62730700
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2023-03-25 22:12:05 +0300

    אייקון אתר + ייעול

---
+++ display.js
@@ -0,0 +1,31 @@
+// get all the list items
+const listItems = document.querySelectorAll("li");
+
+// get the height of the viewport
+const viewportHeight = window.innerHeight;
+
+// listen for scroll events
+window.addEventListener("scroll", () => {
+  // get the scroll position of the page
+  const scrollPosition = window.scrollY;
+
+  // loop through all the list items
+  listItems.forEach((item) => {
+    // get the top and bottom position of the list item
+    const itemTop = item.offsetTop;
+    const itemBottom = item.offsetTop + item.offsetHeight;
+
+    // check if the list item is in the center of the viewport
+    if (itemTop < scrollPosition + viewportHeight / 2 && itemBottom > scrollPosition + viewportHeight / 2) {
+      // set the CSS properties for the highlighted list item
+      item.style.transform = "scale(1.1)";
+      item.style.borderRadius = "10px";
+      item.style.boxShadow = "2px 2px 10px rgba(0,0,0,0.2)";
+    } else {
+      // set the CSS properties for the other list items
+      item.style.transform = "scale(1)";
+      item.style.borderRadius = "5px";
+      item.style.boxShadow = "2px 2px 5px rgba(0,0,0,0.1)";
+    }
+  });
+});
