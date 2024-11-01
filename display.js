638c8e8b25473dc14b284f3a71551acd1557f4bb
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2024-11-01 06:05:41 +0200

    הודעת מדיניות פרטיות

--- display.js
+++ display.js
@@ -35,3 +35,56 @@ function toggleHeaderAnimation() {
 
 window.addEventListener("scroll", handleScroll);
 header.addEventListener("click", toggleHeaderAnimation);
+
+
+  // פונקציה לקריאת ערך של עוגיה לפי שם
+  function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+
+  // פונקציה ליצירת עוגיה
+  function setCookie(name, value, options = {}) {
+    options = {
+      path: '/',
+      // ניתן להוסיף אפשרויות נוספות כאן
+      ...options
+    };
+
+    if (options.expires instanceof Date) {
+      options.expires = options.expires.toUTCString();
+    }
+
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+    for (let optionKey in options) {
+      updatedCookie += "; " + optionKey;
+      let optionValue = options[optionKey];
+      if (optionValue !== true) {
+        updatedCookie += "=" + optionValue;
+      }
+    }
+
+    document.cookie = updatedCookie;
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const cookieBanner = document.getElementById('cookie-banner');
+    const acceptButton = document.getElementById('accept-cookies');
+
+    // בדיקה אם העוגיה כבר קיימת
+    if (!getCookie('cookies_accepted')) {
+      // הצגת ההודעה לאחר השהייה של 1 שניה (ניתן לשנות את הערך)
+      setTimeout(() => {
+        cookieBanner.classList.add('show');
+      }, 1000);
+    }
+
+    // טיפול בלחיצה על כפתור ההסכמה
+    acceptButton.addEventListener('click', function() {
+      setCookie('cookies_accepted', 'yes', { 'max-age': 60 * 60 * 24 * 365 }); // עוגיה לשנה
+      cookieBanner.classList.remove('show');
+    });
+  });
