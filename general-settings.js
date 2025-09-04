0e4c7e549d8b59bf2f9259d3ab93c936aa870f49
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2025-09-04 22:31:26 +0300

    הסרת מעקב GTM

--- general-settings.js
+++ general-settings.js
@@ -1,28 +1,31 @@
 if (window.location.hostname.endsWith('nhlocal.github.io') || window.location.hostname.endsWith('ze-kal.top')) {
+    
     // רישום אירוע עבור ניתוח גוגל
     function conversion_music_drive() {
-      // Send the event to Google Analytics
       gtag('event', 'Registration', {
         'event_category': 'music_drive',
-        'value': 1, // NIS 1
-        'currency': 'NIS' // Currency type
+        'value': 1,
+        'currency': 'NIS'
       });
     };
 
-    // מעקב גוגל
+    // מעקב גוגל (gtag.js) - פעיל
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'G-TGD4VQW4X8');
 
 
+    /* === GTM Snippet - מוער (לא פעיל) ===
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-KQM6HCT');
+    */
 
 
+    // Clarity Snippet - פעיל
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
