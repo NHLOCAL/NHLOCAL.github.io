ca6abe27d0020f412819be4c7f02d624836de430
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2025-09-06 22:30:29 +0300

    Revert "הגדרת GTAG תואם לדומיין"
    This reverts commit 7e84cf28c758f6ad80d30b61ee7fe716d8de17c6.

--- general-settings.js
+++ general-settings.js
@@ -9,32 +9,33 @@ if (window.location.hostname.endsWith('nhlocal.github.io') || window.location.ho
       });
     };
 
-    // ================= מעקב גוגל (gtag.js) =================
+    // ================= מעקב גוגל (gtag.js) - גרסה מלאה ותקינה =================
+    // 1. הגדרת התור (dataLayer) והפונקציה (gtag)
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
+    gtag('config', 'G-TGD4VQW4X8');
 
-    // בחירת Measurement ID לפי דומיין
-    let GA_ID = null;
-    if (window.location.hostname.endsWith('nhlocal.github.io')) {
-        GA_ID = 'G-TGD4VQW4X8'; // NHLOCAL
-    } else if (window.location.hostname.endsWith('ze-kal.top')) {
-        GA_ID = 'G-SPX587LJWB'; // ZE-KAL
-    }
+    // 2. החלק החסר: טעינת ספריית המעקב של גוגל באופן דינמי
+    (function() {
+        var ga = document.createElement('script'); 
+        ga.type = 'text/javascript'; 
+        ga.async = true;
+        ga.src = 'https://www.googletagmanager.com/gtag/js?id=G-TGD4VQW4X8';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+    })();
+    // ===========================================================================
 
-    if (GA_ID) {
-        gtag('config', GA_ID);
 
-        (function() {
-            var ga = document.createElement('script'); 
-            ga.type = 'text/javascript'; 
-            ga.async = true;
-            ga.src = 'https://www.googletagmanager.com/gtag/js?id=' + GA_ID;
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(ga, s);
-        })();
-    }
-    // =========================================================
+    /* === GTM Snippet - מוער (לא פעיל) ===
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KQM6HCT');
+    */
+
 
     // Clarity Snippet - פעיל
     (function(c,l,a,r,i,t,y){
@@ -42,14 +43,4 @@ if (window.location.hostname.endsWith('nhlocal.github.io') || window.location.ho
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "j4f7w6xhpe");
-	
-	
-    /* === GTM Snippet - מוער (לא פעיל) ===
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-KQM6HCT');
-    */	
-	
 }
