d877621447404759600866d6574b46d36113e86b
NHLOCAL <95597943+NHLOCAL@users.noreply.github.com>
2026-06-30 00:29:33 +0300

    Fix GA measurement routing by domain

--- general-settings.js
+++ general-settings.js
@@ -1,46 +1,71 @@
-if (window.location.hostname.endsWith('nhlocal.github.io') || window.location.hostname.endsWith('ze-kal.top')) {
-    
-    // רישום אירוע עבור ניתוח גוגל
-    function conversion_music_drive() {
-      gtag('event', 'Registration', {
-        'event_category': 'music_drive',
-        'value': 1,
-        'currency': 'NIS'
-      });
-    };
+(function () {
+  var host = window.location.hostname.replace(/^www\./, '');
 
-    // ================= מעקב גוגל (gtag.js) - גרסה מלאה ותקינה =================
-    // 1. הגדרת התור (dataLayer) והפונקציה (gtag)
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-TGD4VQW4X8');
+  var measurementByHost = {
+    'nhlocal.github.io': 'G-TGD4VQW4X8',
+    'ze-kal.top': 'G-SPX587LJWB'
+  };
 
-    // 2. החלק החסר: טעינת ספריית המעקב של גוגל באופן דינמי
-    (function() {
-        var ga = document.createElement('script'); 
-        ga.type = 'text/javascript'; 
-        ga.async = true;
-        ga.src = 'https://www.googletagmanager.com/gtag/js?id=G-TGD4VQW4X8';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    })();
-    // ===========================================================================
+  var measurementId = measurementByHost[host];
 
+  if (!measurementId) {
+    return;
+  }
 
-    /* === GTM Snippet - מוער (לא פעיל) ===
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-KQM6HCT');
-    */
+  window.__nhlocalGeneralSettings = window.__nhlocalGeneralSettings || {
+    configuredMeasurementIds: {},
+    loadedScripts: {}
+  };
 
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function () {
+    window.dataLayer.push(arguments);
+  };
 
-    // Clarity Snippet - פעיל
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "j4f7w6xhpe");
-}
+  if (!window.__nhlocalGeneralSettings.loadedScripts[measurementId]) {
+    var existingScript = document.querySelector(
+      'script[src="https://www.googletagmanager.com/gtag/js?id=' + measurementId + '"]'
+    );
+
+    if (!existingScript) {
+      var ga = document.createElement('script');
+      ga.type = 'text/javascript';
+      ga.async = true;
+      ga.src = 'https://www.googletagmanager.com/gtag/js?id=' + measurementId;
+
+      var firstScript = document.getElementsByTagName('script')[0];
+      firstScript.parentNode.insertBefore(ga, firstScript);
+    }
+
+    window.__nhlocalGeneralSettings.loadedScripts[measurementId] = true;
+  }
+
+  if (!window.__nhlocalGeneralSettings.configuredMeasurementIds[measurementId]) {
+    window.gtag('js', new Date());
+    window.gtag('config', measurementId);
+    window.__nhlocalGeneralSettings.configuredMeasurementIds[measurementId] = true;
+  }
+
+  window.conversion_music_drive = function () {
+    window.gtag('event', 'Registration', {
+      event_category: 'music_drive',
+      value: 1,
+      currency: 'NIS'
+    });
+  };
+
+  if (!window.__nhlocalGeneralSettings.clarityLoaded) {
+    (function (c, l, a, r, i, t, y) {
+      c[a] = c[a] || function () {
+        (c[a].q = c[a].q || []).push(arguments);
+      };
+      t = l.createElement(r);
+      t.async = 1;
+      t.src = 'https://www.clarity.ms/tag/' + i;
+      y = l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t, y);
+    })(window, document, 'clarity', 'script', 'j4f7w6xhpe');
+
+    window.__nhlocalGeneralSettings.clarityLoaded = true;
+  }
+})();
